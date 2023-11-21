a0884ba568606c7ca8903936b05f6ceccfc14e01
alexwalland <alexanderwalland@gmail.com>
2023-11-21 17:16:27 +0000

    slider width extended as well as javascript added to update whenever the slider is changed

--- starter/script.js
+++ starter/script.js
@@ -1,110 +1,117 @@
 // Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+let specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+let lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+let upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
-//
+//iniate letiable password
 let password = "";
-let length;
+
+// iniate slider so value can be update when changed and for length of password
+let slider = document.getElementById("range");
+let characters = document.getElementById("length");
+characters.innerHTML = slider.value;
+
+//set length to current value of slider
+let length = slider.value;
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
   //select which character from whichever array is passed in is added to password
-  let j = Math.floor(Math.random()*arr.length);
+  let j = Math.floor(Math.random() * arr.length);
   password += arr[j];
-
 }
 
 // Function to generate password with user input
 function generatePassword() {
   //loop until its reached length and randomise which character should be added to password
-  for (let i=0; i < length; i++) {
-    let a = Math.floor(Math.random()*4);
+  for (let i = 0; i < length; i++) {
+    let a = Math.floor(Math.random() * 4);
     console.log(a);
     switch (a) {
       case 0:
@@ -118,23 +125,29 @@ function generatePassword() {
         break;
       case 3:
         getRandom(specialCharacters);
-        break;  
-    } 
+        break;
+    }
   }
 }
 
 // Get references to the #generate element
-let generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  length = 8;
+  //set length to slider value
+  length = slider.value;
   generatePassword();
-  let passwordText = document.querySelector('#password');
+  let passwordText = document.querySelector("#password");
   console.log(password);
   passwordText.value = password;
   password = "";
 }
 
 // Add event listener to generate button and generate password
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
+
+//update length 
+slider.oninput = function() {
+  characters.innerHTML = this.value;
+}
